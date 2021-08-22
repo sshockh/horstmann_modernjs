fdc1d5a82ecb9d9f09d1f7b0ca76ad61ecc1c21e
sshock <sshock@mail.ru>
2021-08-22 21:40:25 +0300

    итераторы. зад.5

--- 12_generators/task_05.js
+++ 12_generators/task_05.js
@@ -1,5 +1,22 @@
 class Range {
     constructor(start, end) {
-        
+        this.start = start;
+        this.end = end;
+    }
+    [Symbol.iterator]() {
+        let current = this.start;
+        const end = this.end;
+        return {
+            next() {
+                current++;
+                return current <= end
+                    ? { value: current - 1 }
+                    : { done: true }
+            }
+        }
     }
 }
+
+const iterable = new Range(1, 6);
+for (let el of iterable)
+    console.log(el);
